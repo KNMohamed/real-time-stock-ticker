b8b54d12899f59aecd424d8d3df144256d5e969c
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-10-10 16:32:45 -0400

    Add fonts; Create change logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { useEffect, useState } from 'react';
+import protobuf from 'protobufjs';
+
+const { Buffer } = require('buffer/');
+
+const emojis = {
+  '': '',
+  up: '🚀',
+  down: '💩',
+};
+
 function App() {
+  const [stock, setStock] = useState(null);
+  const [direction, setDirection] = useState('');
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    protobuf.load('./YPricingData.proto', (error, root) => {
+      const Yaticker = root.lookupType('yaticker');
+
+      ws.onopen = function open() {
+        console.log('connected');
+        ws.send(
+          JSON.stringify({
+            subscribe: ['GME'],
+          }),
+        );
+      };
+
+      ws.onclose = function close() {
+        console.log('disconnected');
+      };
+
+      ws.onmessage = function incoming(message) {
+        const next = Yaticker.decode(Buffer.from(message.data, 'base64'));
+        setStock(curr => {
+          if (curr) {
+            const nextDirection =
+              curr.price < next.price
+                ? 'up'
+                : curr.price > next.price
+                ? 'down'
+                : '';
+            if (nextDirection) {
+              console.log(nextDirection);
+              setDirection(nextDirection);
+            }
+          }
+          return next;
+        });
+      };
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Stonks</h1>
+      <div className="resultMarquee">
+        {stock && (
+          <h2>
+            {stock.id}:{' '}
+            <span>
+              {' '}
+              ${stock.price.toFixed(2)} {emojis[direction]}
+            </span>
+          </h2>
+        )}
+      </div>
     </div>
   );
 }
